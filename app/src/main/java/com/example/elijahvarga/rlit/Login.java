6728cf2112e1e0b1a7f1636f466861f8802152af
Nehushtan135 <elijahvarga@gmail.com>
2015-10-07 13:48:04 -0700

    Pushing finish on login page now goes to main page yay :)

--- app/src/main/java/com/example/elijahvarga/rlit/Login.java
+++ app/src/main/java/com/example/elijahvarga/rlit/Login.java
@@ -1,16 +1,34 @@
 package com.example.elijahvarga.rlit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        initLogin();
+    }
+
+    private void initLogin() {
+        loginButton = (Button) findViewById(R.id.finish);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Implement authentication here
+                Intent i = new Intent(getApplicationContext(),HomepageActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 
     @Override
