f43d8a1b91fd48476be6bafb12578c441d9c8b08
Nehushtan135 <elijahvarga@gmail.com>
2015-10-09 12:03:15 -0700

    Pushing finish on login page now goes to main page yay :)

--- app/src/main/java/com/example/elijahvarga/rlit/dayByDay.java
+++ app/src/main/java/com/example/elijahvarga/rlit/dayByDay.java
@@ -1,16 +1,41 @@
 package com.example.elijahvarga.rlit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class dayByDay extends AppCompatActivity {
+
+    private Button greenDayButton;
+    private Button redDayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_by_day);
+        initDayByDay();
+    }
+
+    private void initDayByDay() {
+        greenDayButton = (Button) findViewById(R.id.greenDayButton);
+        greenDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO do whatever greenButton will do
+            }
+        });
+
+        redDayButton = (Button) findViewById(R.id.redDayButton);
+        redDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO do what redButton will do
+            }
+        });
     }
 
     @Override
